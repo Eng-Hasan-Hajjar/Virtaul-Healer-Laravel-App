781955270f123fd3b517fd2bdb9ac40efb72f347
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-01-30 02:39:18 -0800

    add new model and migration for  question skin

---
+++ app/Models/QuestionsSkin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionsSkin extends Model
+{
+    use HasFactory;
+}
