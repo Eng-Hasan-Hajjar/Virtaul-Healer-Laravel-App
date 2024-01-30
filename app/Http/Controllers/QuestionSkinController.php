085241801e7b2bc73f0fd2edea78c28b711a751a
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-01-30 02:37:19 -0800

    add new controller question skin

---
+++ app/Http/Controllers/QuestionSkinController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\igration;
+use Illuminate\Http\Request;
+
+class QuestionSkinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(igration $igration)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(igration $igration)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, igration $igration)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(igration $igration)
+    {
+        //
+    }
+}
