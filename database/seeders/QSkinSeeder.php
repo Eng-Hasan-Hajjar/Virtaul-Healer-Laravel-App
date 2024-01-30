3fccdf76393320e7037e56ef7fb0e012db921910
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-01-30 03:35:10 -0800

    add new questions into seeder  for  question skin

--- database/seeders/QSkinSeeder.php
+++ database/seeders/QSkinSeeder.php
@@ -12,6 +12,45 @@ class QSkinSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        \App\Models\QuestionsSkin::create([
+            'question' => 'هل لاحظت تغييرًا مفاجئًا في لون أو شكل أي جزء من بشرتك؟',
+              'answer_type' => 'YesNo',
+         ]);
+         \App\Models\QuestionsSkin::create([
+            'question' => 'هل تعاني من حكة شديدة أو ألم في الجلد؟',
+              'answer_type' => 'YesNo',
+         ]);
+         \App\Models\QuestionsSkin::create([
+            'question' => 'هل قمت بتغيير مستحضرات العناية بالبشرة أو استخدمت منتجات جديدة مؤخرًا؟',
+              'answer_type' => 'YesNo',
+         ]);
+         \App\Models\QuestionsSkin::create([
+            'question' => 'هل كنت تتعرض لأشعة الشمس بشكل مكثف دون استخدام واقي الشمس؟',
+              'answer_type' => 'YesNo',
+         ]);
+         \App\Models\QuestionsSkin::create([
+            'question' => 'هل تعاني من أمراض تحسسية أو حساسية جلدية معروفة؟',
+              'answer_type' => 'YesNo',
+         ]);
+         \App\Models\QuestionsSkin::create([
+            'question' => 'هل لديك تاريخ عائلي لأمراض جلدية؟',
+              'answer_type' => 'YesNo',
+         ]);
+         \App\Models\QuestionsSkin::create([
+            'question' => 'هل تعاني من أمراض مزمنة أخرى مثل السكري أو الأمراض الروماتيزمية؟',
+              'answer_type' => 'YesNo',
+         ]);
+         \App\Models\QuestionsSkin::create([
+            'question' => 'هل تشعر بأي تغير في درجة الحرارة أو الرطوبة يؤثر على حالة بشرتك؟',
+              'answer_type' => 'YesNo',
+         ]);
+         \App\Models\QuestionsSkin::create([
+            'question' => 'هل قمت بزيارة أماكن جديدة أو تعرضت للحشرات مؤخرًا؟',
+              'answer_type' => 'YesNo',
+         ]);
+         \App\Models\QuestionsSkin::create([
+            'question' => 'هل لاحظت تفاقم الأعراض بعد تناول أطعمة أو مشروبات معينة؟',
+              'answer_type' => 'YesNo',
+         ]);
     }
 }
