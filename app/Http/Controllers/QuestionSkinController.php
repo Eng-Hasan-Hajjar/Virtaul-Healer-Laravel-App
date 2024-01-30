6a142895a3526b10b61536a910546a5e33c42ec5
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-01-30 02:53:16 -0800

    add instructions to  QuestionSkinController  for  question skin

--- app/Http/Controllers/QuestionSkinController.php
+++ app/Http/Controllers/QuestionSkinController.php
@@ -2,25 +2,143 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\igration;
+use App\Models\Diagnosis;
+use App\Models\QuestionsSkin;
 use Illuminate\Http\Request;
 
 class QuestionSkinController extends Controller
 {
-    /**
+  /**
+     * Display a listing of the resource.
+     */
+    public function index2()
+    {
+        $questionsSkin = QuestionsSkin::all();
+        return view('questions.skin.index', compact('questionsSkin'));
+
+    }
+    public function submitStomach(Request $request)
+    {
+          $description = "";
+          $description1 = "";
+          $description2 = "";
+          $description3 = "";
+          $description4 = "";
+          $description5 = "";
+          $description6 = "";
+          $description7 = "";
+
+          $questionsSkincount=QuestionsSkin::all()->count();
+          $answers = $request->all();
+
+          $yesAnswersCount = 0;
+          $noAnswersCount = 0;
+          // قم بتلقي البيانات من النموذج
+            $answers = $request->all();
+            // استرجاع الأسئلة من قاعدة البيانات
+            $questionsSkin = QuestionsSkin::all();
+
+            // تفاصيل الأسئلة
+            foreach ($questionsSkin as $question) {
+                $answer = $answers['answer_' . $question->id] ?? 'لم يتم الإجابة';
+
+                if($question->id == 1 && $answer == 'No' ){   $description1=  " " . "<br><br>"; }
+                else  if($question->id == 1 && $answer == 'Yes' ){$description1= "". "<br><br>";}
+
+                if($question->id == 2 && $answer == 'No' ){  $description2=  "<br><br>"; }
+                else if($question->id == 2 && $answer == 'Yes' ) {$description2=  "<br><br>";}
+
+                if($question->id == 3 && $answer == 'No' ){  $description3=  "<br><br>"; }
+                else  if($question->id == 3 && $answer == 'Yes' ){$description3=  "<br><br>";}
+
+                if($question->id == 4 && $answer == 'No' ){   $description4=  "<br><br>"; }
+                else  if($question->id == 4 && $answer == 'Yes' ){$description4=  "<br><br>";}
+
+                if($question->id == 5 && $answer == 'No' ){  $description5= "<br><br>"; }
+                else  if($question->id == 5 && $answer == 'Yes' ){$description5=   "<br><br>";}
+
+                if($question->id == 6 && $answer == 'No' ){    $description6=  "<br><br>"; }
+                else  if($question->id == 6 && $answer == 'Yes' ){$description6=   "<br><br>";}
+
+            }
+
+
+            $description = $description1.$description2.$description3.$description4.$description5.$description6;
+            foreach ($answers as $answer) {
+                if ($answer == 'Yes') {
+                    $yesAnswersCount++;
+                }
+                else {
+                    $noAnswersCount++;
+                }
+            }
+
+            if ($yesAnswersCount >= 2) {
+                if ($yesAnswersCount >= 3) {
+                    if ($yesAnswersCount >= 4) {
+                        $diagnosis = "انك تعاني من حالة شديدة. من الضروري الاتصال بالطبيب الخاص بك فوراً.";
+                    } else {
+                        $diagnosis = "   لديك مشكلة   . يُفضل استشارةالطبيب الخاص بك.";
+                    }
+                }else{
+                    $diagnosis = "   لديك مشكلة في  التنفسي. ";
+                }
+            } else {
+                $diagnosis = "لا يبدو أن هناك حالة خطيرة. يمكنك متابعة وضعك، ولكن إذا كانت هناك أية مشاكل، يجب عليك استشارة الطبيب.";
+            }
+
+            $yesAnswersCountpercent= $yesAnswersCount * 100 / $questionsSkincount ;
+
+            if ($yesAnswersCountpercent >= 20) {
+                if ($yesAnswersCountpercent >= 30) {
+                    if ($yesAnswersCountpercent >= 40) {
+                        if ($yesAnswersCountpercent >= 50) {
+                            if ($yesAnswersCountpercent >= 60) {
+                                if ($yesAnswersCountpercent >= 70) {
+                                    if ($yesAnswersCountpercent >= 80) {
+                                        if ($yesAnswersCountpercent >= 90) {
+                                                           $diagnosisdb = Diagnosis::find(12);
+                                        }
+                                             $diagnosisdb = Diagnosis::find(11);
+                                    }
+                                         $diagnosisdb = Diagnosis::find(10);
+                                }
+                                     $diagnosisdb = Diagnosis::find(10);
+                            }
+                                 $diagnosisdb = Diagnosis::find(9);
+                        }
+                             $diagnosisdb = Diagnosis::find(9);
+                    }
+                         $diagnosisdb = Diagnosis::find(9);
+                }
+                     $diagnosisdb = Diagnosis::find(8);
+            }
+
+            $id = $diagnosisdb->id;
+            $descriptiondb = $diagnosisdb->description;
+
+
+            return view('outdescription', compact('description','diagnosis','descriptiondb','yesAnswersCount','yesAnswersCountpercent'));
+
+
+    }
+
+   /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
+        $questionsSkins = QuestionsSkin::latest()->paginate(5);
 
+        return view('doctor.crudquestions.skin.index',compact('questionsSkins'))
+                    ->with('i', (request()->input('page', 1) - 1) * 2);
+    }
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        return view('doctor.crudquestions.skin.create');
     }
 
     /**
@@ -28,38 +146,58 @@ class QuestionSkinController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'question' => 'required',
+
+        ]);
+
+        QuestionsSkin::create($request->all());
+
+        return redirect()->route('questionsSkin.index')
+                        ->with('success','questionsSkin created successfully.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(igration $igration)
+    public function show(QuestionsSkin $questionsSkin)
     {
-        //
+        return view('doctor.crudquestions.skin.show',compact('questionsSkin'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(igration $igration)
+    public function edit(QuestionsSkin $questionsSkin)
     {
-        //
+        return view('doctor.crudquestions.skin.edit',compact('questionsSkin'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, igration $igration)
+    public function update(Request $request, QuestionsSkin $questionsSkin)
     {
-        //
+        $request->validate([
+            'question' => 'required',
+
+        ]);
+
+        $questionsSkin->update($request->all());
+
+        return redirect()->route('questionsSkin.index')
+                        ->with('success','questionsSkin updated successfully');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(igration $igration)
+    public function destroy(QuestionsSkin $questionsSkin)
     {
-        //
+        $questionsSkin->delete();
+
+        return redirect()->route('questionsSkin.index')
+                        ->with('success','questionsSkin deleted successfully');
     }
+
 }
