300fdba10a136a954d55be46fead1d39409676ee
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-01-30 02:42:33 -0800

    add instructions to  model and migration for  question skin

--- app/Models/QuestionsSkin.php
+++ app/Models/QuestionsSkin.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class QuestionsSkin extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'question',
+        'answer_type',
+    ];
 }
--- database/migrations/2024_01_30_103836_create_questions_skins_table.php
+++ database/migrations/2024_01_30_103836_create_questions_skins_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('questions_skins', function (Blueprint $table) {
             $table->id();
+            $table->string('question');
+            $table->enum('answer_type', ['YesNo', 'Text']); // نوع الإجابة
+
             $table->timestamps();
         });
     }
